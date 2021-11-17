daff9ed2a12b242a0ac53804946068e9a53eba18
Michał Król <mharnen@gmail.com>
2021-11-17 20:21:33 +0000

    Create server_tcp.py

---
+++ server_tcp.py
@@ -0,0 +1,40 @@
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+port = int(sys.argv[1])
+server_address = ('0.0.0.0', port)
+print('starting up on {} port {}'.format(*server_address))
+sock.bind(server_address)
+
+# Listen for incoming connections
+sock.listen(1)
+
+while True:
+    # Wait for a connection
+    print('waiting for a connection')
+    connection, client_address = sock.accept()
+    try:
+        print('connection from', client_address)
+
+        # Receive the data in small chunks and retransmit it
+        while True:
+            try:
+                data = connection.recv(16)
+            except:
+                print("An error occured")
+                break
+            print('received {!r}'.format(data))
+            if data:
+                print('sending data back to the client')
+                connection.sendall(data)
+            else:
+                print('no data from', client_address)
+                break
+
+    finally:
+        # Clean up the connection
+        connection.close()
